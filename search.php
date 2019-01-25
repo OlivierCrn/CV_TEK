6fddf38e09e96d7b9f065b58d87f89187d2d68b8
noethix <41691381+noethix@users.noreply.github.com>
2019-01-25 10:59:48 +0100

    Add files via upload

--- search.php
+++ search.php
@@ -1,28 +1,105 @@
 <?php
 
 function search($candidats)
+
 {
 
+function LastName($candidats) 
+{
+    $saisie=cleanInput(readline("Nom : "));
+    $resultats=array();
+    $compt_resultats=1;
 
-affichage($candidats);
-
-
-$saisie=readline("Nom : ");
-$resultats=array();
-$compt_resultats=1;
-
-for ($i=1;$i<=count($candidats);$i++) {
-    if ($saisie==$candidats[$i]["LastName"]) {
+for ($i=1;$i<=count($candidats);$i++) 
+    {
+    if ($saisie==$candidats[$i]["LastName"]) 
+        {
         $resultats[$compt_resultats]=$candidats[$i];
         $compt_resultats++;
+        }
     }
-
+affichage($resultats);    
 }
 
-print_r($resultats);
 
-affichageC($resultats);
+function Ages($candidats)
+{
+    $saisie=cleanInput(readline("Age : "));
+    $resultats=array();
+    $compt_resultats=1;
+
+for ($i=1; $i<=count($candidats); $i++) 
+    {
+    if ($saisie==$candidats[$i]["Age"]) 
+        {
+        $resultats[$compt_resultats]=$candidats[$i];
+        $compt_resultats++;
+        }
+    }
+affichage($resultats);
+}
+
+function Town($candidats) 
+{
+    $saisie=cleanInput(readline("Ville : "));
+    $resultats=array();
+    $compt_resultats=1;
+for ($i=1; $i<=count($candidats); $i++) 
+    {
+    if($saisie==$candidats[$i]["Town"]) 
+        {
+        $resultats[$compt_resultats]=$candidats[$i];
+        $compt_resultats++;
+        }
+    }
+affichage($resultats);   
+}
 
 
+function Skills($candidiats) 
+{
+    $saisie=cleanInput(readline("Competences : "));
+    $resultats=array();
+    $compt_resultats=1;
+for ($i=1; $i<=count($candidats); $i++) 
+    {
+    if ($saisie==$candidats[$i]["Skills"]) 
+        {
+        $resultats[$compt_resultats]=$candidats[$i];
+        $compt_resultats++;
+        }
+    }
+affichage($resultats);    
+}
+
+
+
+//affichage($candidats);
+print "Que voulez vous chercher?". PHP_EOL;
+print PHP_EOL;
+print "1 - Rechercher par nom". PHP_EOL;
+print "2 - Rechercher par Age". PHP_EOL;
+print "3 - Rechercher par Ville".PHP_EOL;
+print "4 - Rechercher par Competences". PHP_EOL;
+print "5 - Retour au menu principal". PHP_EOL;
+print PHP_EOL;
+$REsearch = readline("Votre choix : ");
+
+switch ($REsearch) {
+    case "1":
+    LastName($candidats);
+
+    case "2": 
+    Ages($candidats);
+
+    case "3":
+    Town($candidats);
+
+    case "4":
+    Skills($candidats);
+
+    case "5":
+    return;
+}
 }
 ?>
