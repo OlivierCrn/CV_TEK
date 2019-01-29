8dee602992f515c8f9a91ea01e70aae2400d0b07
noethix <41691381+noethix@users.noreply.github.com>
2019-01-29 17:09:53 +0100

    Update and rename menu_fr.php to MainMenu.php

--- MainMenu.php
+++ MainMenu.php
@@ -1,10 +1,13 @@
 <?php
 //Call
+
+$defaultLanguage = "FR";
 require_once('function.php');
 require_once('list.php');
 require_once('search.php');
 require_once('add.php');
 require_once('modification.php');
+require_once('traductionFR.php');
 //Variables
 $ID = 0;
 $IDs = 0;
@@ -17,37 +20,38 @@ $candidats=csv_to_array('hrdata.csv');
 $compteur= 0;
 //Start
 //General menu:
-function menu_FR($candidats)
+function menu_FR($candidats,$traduction)
 {
+	$informations=array();
     print PHP_EOL;
-    print("Menu, ou voulez vous aller? "). PHP_EOL;
+    print $traduction["MAIN_MENU1"]. PHP_EOL;
     print PHP_EOL;
-    print "1 - Liste des candidats ". PHP_EOL;
-    print "2 - Ajout de candidat ". PHP_EOL;
-    print "3 - Modification des informations d'un candidat ". PHP_EOL;
-    print "4 - Recherche  ". PHP_EOL;
-    print "5 - Quitter ". PHP_EOL;
-    $menu=readline("Votre choix: ");
+    print $traduction["MAIN_MENU2"]. PHP_EOL;
+    print $traduction["MAIN_MENU3"]. PHP_EOL;
+    print $traduction["MAIN_MENU4"]. PHP_EOL;
+    print $traduction["MAIN_MENU5"]. PHP_EOL;
+    print $traduction["MAIN_MENU6"]. PHP_EOL;
+    $menu=readline($traduction["MENU_CHOICE"]);
     print  PHP_EOL;
     switch ($menu) {
         case "1":
         li($candidats);
-        menu_FR($candidats);
+        menu_FR($candidats,$traduction);
         case "2":
-        $candidats[]=add($informations);
-        menu_FR($candidats);
+        $candidats=add($candidats,$traduction);
+        menu_FR($candidats,$traduction);
         case "3":
         $candidats=modificationCandidats($candidats);
-        menu_FR($candidats);
+        menu_FR($candidats,$traduction);
         case "4":   
         search($candidats);
-        menu_FR($candidats);
+        menu_FR($candidats,$traduction);
         case "5":
         quit($candidats);
         default:
-        print "Choisir un chiffre entre 1 et 5". PHP_EOL;
+        print $traduction["MAIN_MENU_DEFAULT"]. PHP_EOL;
         print PHP_EOL;
-        menu_FR($candidats);
+        menu_FR($candidats,$traduction);
     }
 }
 
@@ -56,12 +60,12 @@ function quit($candidats)
 {
 
     print PHP_EOL;
-    print "Voulez-vous vraiment quitter le programme ? y/N ";
+    print $traduction["MENU_QUIT1"];
     $YesNo = readline();
     $YesNo = strtoupper($YesNo);
         if($YesNo[0] == "Y")
         {
-            print "Vous quittez le programme". PHP_EOL;
+            print $traduction["MENU_QUIT2"]. PHP_EOL;
             exit;
         }
         else
@@ -71,5 +75,5 @@ function quit($candidats)
     
 }
 
-menu_FR($candidats)
+menu_FR($candidats,$traduction)
 ?>
