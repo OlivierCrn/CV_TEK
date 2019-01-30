1eb0fe88338daa250f592a54edee42e197ded385
noethix <41691381+noethix@users.noreply.github.com>
2019-01-30 09:51:43 +0100

    Update MainMenu.php

--- MainMenu.php
+++ MainMenu.php
@@ -1,27 +1,17 @@
 <?php
 //Call
-
-$defaultLanguage = "FR";
-require_once('function.php');
 require_once('list.php');
 require_once('search.php');
 require_once('add.php');
 require_once('modification.php');
-require_once('traductionFR.php');
-//Variables
-$ID = 0;
-$IDs = 0;
-$saisie=0;
-$validation = false;
-$nul = "NULL";
-$Skills=array();
-$informations=array();
-$candidats=csv_to_array('hrdata.csv');
-$compteur= 0;
+require_once('Translation'.$default_language);
+
 //Start
 //General menu:
-function menu_FR($candidats,$traduction)
+function MainMenu($candidats,$traduction)
 {
+
+    
 	$informations=array();
     print PHP_EOL;
     print $traduction["MAIN_MENU1"]. PHP_EOL;
@@ -35,28 +25,28 @@ function menu_FR($candidats,$traduction)
     print  PHP_EOL;
     switch ($menu) {
         case "1":
-        li($candidats);
-        menu_FR($candidats,$traduction);
+        li($candidats, $traduction);
+        MainMenu($candidats,$traduction);
         case "2":
         $candidats=add($candidats,$traduction);
-        menu_FR($candidats,$traduction);
+        MainMenu($candidats,$traduction);
         case "3":
-        $candidats=modificationCandidats($candidats);
-        menu_FR($candidats,$traduction);
+        $candidats=modificationCandidats($candidats, $traduction);
+        MainMenu($candidats,$traduction);
         case "4":   
-        search($candidats);
-        menu_FR($candidats,$traduction);
+        search($candidats, $traduction);
+        MainMenu($candidats,$traduction);
         case "5":
-        quit($candidats);
+        quit($candidats, $traduction);
         default:
         print $traduction["MAIN_MENU_DEFAULT"]. PHP_EOL;
         print PHP_EOL;
-        menu_FR($candidats,$traduction);
+        MainMenu($candidats,$traduction);
     }
 }
 
 //End
-function quit($candidats) 
+function quit($candidats, $traduction) 
 {
 
     print PHP_EOL;
@@ -65,15 +55,14 @@ function quit($candidats)
     $YesNo = strtoupper($YesNo);
         if($YesNo[0] == "Y")
         {
+            write ($candidats);
             print $traduction["MENU_QUIT2"]. PHP_EOL;
             exit;
         }
         else
         {
-            menu_fr($candidats);
+            MainMenu($candidats, $traduction);
         }
     
 }
-
-menu_FR($candidats,$traduction)
 ?>
